c091940d9722db6551251d4bd167d3892e0fad10
Michael <potopaev.michael@gmail.com>
2024-02-02 14:59:32 +0600

    созданы точки входа и представляения для действий с числами. задание 1 завершено.

--- source/api_1/urls.py
+++ source/api_1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
+from api_1.views import add_view, subtract_view, multiply_view, divide_view
 
 app_name = 'api_1'
 
 urlpatterns = [
-    path('add/', ),
-    path('substract/', ),
-    path('multiply/', ),
-    path('divide/', ),
+    path('add/', add_view, name='add_result' ),
+    path('substract/', subtract_view, name='subtract_result'),
+    path('multiply/', multiply_view, name='multiply_result'),
+    path('divide/', divide_view, name='divide_result'),
 ]
--- source/api_1/views.py
+++ source/api_1/views.py
@@ -1,3 +1,84 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+import json
+
+
+def check_input(dict):
+    if isinstance(dict.get('A'), (int, float)) and isinstance(dict.get('B'), (int, float)):
+        a = dict.get('A')
+        b = dict.get('B')
+        return a, b
+    else:
+        raise Exception('A and B must be digits!')
+
 
 # Create your views here.
+def add_view(request, *args, **kwargs):
+    answer = {}
+    if request.body:
+        answer = json.loads(request.body)
+    else:
+        return JsonResponse({'error': 'No data provided'}, status=400)
+
+    try:
+        a, b = check_input(answer)
+        result = {
+            "answer": a + b,
+        }
+        return JsonResponse(result, safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
+
+def subtract_view(request, *args, **kwargs):
+    answer = {}
+    if request.body:
+        answer = json.loads(request.body)
+    else:
+        return JsonResponse({'error': 'No data provided'}, status=400)
+
+    try:
+        a, b = check_input(answer)
+        result = {
+            "answer": a - b,
+        }
+        return JsonResponse(result, safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
+
+def multiply_view(request, *args, **kwargs):
+    answer = {}
+    if request.body:
+        answer = json.loads(request.body)
+    else:
+        return JsonResponse({'error': 'No data provided'}, status=400)
+
+    try:
+        a, b = check_input(answer)
+        result = {
+            "answer": a * b,
+        }
+        return JsonResponse(result, safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
+
+def divide_view(request, *args, **kwargs):
+    answer = {}
+    if request.body:
+        answer = json.loads(request.body)
+    else:
+        return JsonResponse({'error': 'No data provided'}, status=400)
+
+    try:
+        a, b = check_input(answer)
+        if b == 0:
+            return JsonResponse({'error': 'Division by zero!'}, status=400)
+        else:
+            result = {
+                "answer": a / b,
+            }
+            return JsonResponse(result, safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
